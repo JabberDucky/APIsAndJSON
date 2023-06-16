697c423a46df4d04db8f27c905afa9724bd5d044
Katherine Vail <jabberduckygame@gmail.com>
2023-06-16 13:39:50 -0500

    Completed exercise

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace APIsAndJSON
+﻿using System.Text.Json.Nodes;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+             
+            for(int i = 0; i < 5; i++) 
+            {
+                RonVSKanyeAPI.Kanye();
+                RonVSKanyeAPI.Ron();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("What's the weather like?");
+            OpenWeatherMapAPI.CurrentWeather();
+            Console.ReadLine();
         }
+
     }
 }
